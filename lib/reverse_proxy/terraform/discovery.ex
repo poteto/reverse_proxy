0fbf819522164f332ed9a5817ef8429bc28489fa
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2016-08-07 22:15:34 -0700

    Clean up macro

--- lib/reverse_proxy/terraform/discovery.ex
+++ lib/reverse_proxy/terraform/discovery.ex
@@ -17,16 +17,21 @@ defmodule ReverseProxy.Terraform.Discovery do
         try do
           super(conn, opts)
         catch
-          _, %Phoenix.Router.NoRouteError{conn: %{method: method, request_path: request_path} = conn} ->
-            verb =
-              method
-              |> String.downcase()
-              |> String.to_atom()
-
-            Logger.debug("Forwarding request to #{@terraformer}.#{verb}/2")
-            apply(@terraformer, verb, [request_path, conn])
+          _, %Phoenix.Router.NoRouteError{conn: conn} -> terraform(conn, @terraformer)
         end
       end
+
+      defp terraform(%{method: method, request_path: request_path} = conn, terraformer) do
+        verb =
+          method
+          |> String.downcase()
+          |> String.to_atom()
+
+        Logger.debug("Forwarding request to #{terraformer}.#{verb}/2")
+        apply(terraformer, verb, [request_path, conn])
+      end
+
+      defoverridable [terraform: 2]
     end
   end
 end
