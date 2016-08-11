f08272c15a2c8a18aae07d2a560a9198bf156536
Dan McClain <git@danmcclain.net>
2016-08-11 19:04:08 -0400

    Use Plug.Router (#5)

--- lib/reverse_proxy/terraformers/giphy.ex
+++ lib/reverse_proxy/terraformers/giphy.ex
@@ -1,27 +1,23 @@
 defmodule ReverseProxy.Terraformers.Giphy do
   alias ReverseProxy.Clients.Giphy
-  import Plug.Conn, only: [send_resp: 3]
+  use Plug.Router
 
-  def init(opts), do: opts
+  plug :match
+  plug :dispatch
 
   # match specific path
-  def call(%{method: "GET", request_path: "/v1/hello-world"} = conn, _) do
+  get "/v1/hello-world" do
     send_resp(conn, 200, "Hello world")
   end
+
   # catch all `get`s
-  def call(%{method: "GET", request_path: request_path, params: params, req_headers: req_headers} = conn, _) do
+  get _ do
+    %{method: "GET", request_path: request_path, params: params, req_headers: req_headers} = conn
     res = Giphy.get!(request_path, req_headers, [params: Map.to_list(params)])
     send_response({:ok, conn, res})
   end
 
-  def call(%{method: "PUT"}, _),      do: raise "Not implemented yet"
-  def call(%{method: "PATCH"}, _),    do: raise "Not implemented yet"
-  def call(%{method: "POST"}, _),     do: raise "Not implemented yet"
-  def call(%{method: "OPTIONS"}, _),  do: raise "Not implemented yet"
-  def call(%{method: "DELETE"}, _),   do: raise "Not implemented yet"
-  def call(%{method: "HEAD"}, _),     do: raise "Not implemented yet"
-  def call(%{method: "TRACE"}, _),    do: raise "Not implemented yet"
-  def call(%{method: "CONNECT"}, _),  do: raise "Not implemented yet"
+  match _, do: raise "Not implemented yet"
 
   defp send_response({:ok, conn, %{headers: headers, status_code: status_code, body: body}}) do
     conn = %{conn | resp_headers: headers}
